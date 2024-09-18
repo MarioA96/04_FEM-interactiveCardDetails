7ed92acfd36db7a1b78215550ebcb7203a256874
MarioA96 <marioaquirozs@gmail.com>
2024-09-18 14:55:25 -0600

    Se genero una interfaz para los contenido de CardContent

---
+++ src/interfaces/CardContent.ts
@@ -0,0 +1,28 @@
+export interface CardContent {
+    backgrounds:    Background;
+    backsideCard:  BacksideCard;
+    frontsideCard: FrontsideCard;
+}
+
+export interface Background {
+    background_1040:    string;
+    background_640:     string;
+    background_340:     string;
+    background_regular: string;
+}
+
+export interface BacksideCard {
+    media: string;
+    value: string;
+}
+
+export interface FrontsideCard {
+    cardNumber: string;
+    holderName: string;
+    expDate:    ExpDate;
+}
+
+export interface ExpDate {
+    month: string;
+    year:  string;
+}

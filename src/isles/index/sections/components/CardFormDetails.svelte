cb292efa9d4b55f67ce44acf20a0589f12fa0337
MarioA96 <marioaquirozs@gmail.com>
2024-09-25 15:36:52 -0600

    Se creo un componente de sección CardFormDetails.svelte

---
+++ src/isles/index/sections/components/CardFormDetails.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import FormSection from "./FormSection.svelte";
+    import SuccesfulCardDetails from "./SuccesfulCardDetails.svelte";
+
+    type FrontsideCardDetails = {
+        holderName: string;
+        cardNumber: string;
+        expDate: {
+            month: string;
+            year: string;
+        };
+    }
+
+    type BacksideCardDetails = {
+        cvc: string;
+    }
+
+    export let frontsideCardDetails: FrontsideCardDetails;
+    export let backsideCardDetails: BacksideCardDetails;
+    let isValidForm: boolean = false;
+
+</script>
+
+{#if isValidForm}
+    <SuccesfulCardDetails bind:isValidForm={isValidForm}/>
+{:else}
+    <FormSection 
+        frontsideCardDetails={frontsideCardDetails} 
+        backsideCardDetails={backsideCardDetails} 
+        bind:isValidForm={isValidForm}
+    />
+{/if}
